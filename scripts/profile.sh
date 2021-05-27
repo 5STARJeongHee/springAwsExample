f97fb000016d157acc528f37846ad1fa0f716325
5STARJeongHee <qhxmaoflr@gmail.com>
2021-05-27 18:05:00 +0900

    무중단 배포를 위한 script, properties 추가, 버전 명 날짜를 추가해 자동으로 버전이 바뀌고 겹치지 않도록함,

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,36 +1,39 @@
 #!/usr/bin/env bash
 # 쉬고 있는 profile 찾기: real1 사용중일 경우 real2가 쉬고, 반대일 경우 real1이 쉬고, real1이 살아 있음
+# -s 진행내역, 메시지 출력x, -o 원격데이터를 /dev/null로보내 아무런 출력 x, http_code- response code(200인가 아닌가)
 
-fucntion find_idle_profile()
+# bash는 return value가 안되니 *제일 마지막줄에 echo로 해서 결과 출력*후, 클라이언트에서 값을 사용한다
+
+function find_idle_profile()
 {
-  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}\n" http://localhost/profile)
-  # -s 진행내역, 메시지 출력x, -o 원격데이터를 /dev/null로보내 아무런 출력 x, http_code- response code(200인가 아닌가)
-  if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 에러로 봄 - 정상코드 200
-  then
-    CURRENT_PROFILE=real2
-  else
-    CURRENT_PROFILE=$(curl -s http://localhost/profile)
-  fi
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-  if [[${CURRENT_PROFILE} == real1 ]]
-  then
-    IDLE_PROFILE=real2
-  else
-    IDLE_PROFILE=real1
-  fi
+    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
+    then
+        CURRENT_PROFILE=real2
+    else
+        CURRENT_PROFILE=$(curl -s http://localhost/profile)
+    fi
 
-  echo "${IDLE_PROFILE}"
+    if [ ${CURRENT_PROFILE} == real1 ]
+    then
+      IDLE_PROFILE=real2
+    else
+      IDLE_PROFILE=real1
+    fi
+
+    echo "${IDLE_PROFILE}"
 }
 
 # 쉬고 있는 profile의 port 찾기
 function find_idle_port()
 {
-  IDLE_PROFILE=$(find_idle_profile)
+    IDLE_PROFILE=$(find_idle_profile)
 
-  if [[${IDLE_PROFILE} == real1 ]]
-  then
-    echo "8081"
-  else
-    echo "8082"
-  fi
+    if [ ${IDLE_PROFILE} == real1 ]
+    then
+      echo "8081"
+    else
+      echo "8082"
+    fi
 }
