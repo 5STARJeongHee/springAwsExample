52f688603269994fdb4d6b5896c70706e8b6f786
5STARJeongHee <qhxmaoflr@gmail.com>
2021-05-27 17:47:14 +0900

    무중단 배포를 위한 script, properties 추가, 버전 명 날짜를 추가해 자동으로 버전이 바뀌고 겹치지 않도록함,

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -5,16 +5,16 @@ function find_idle_profile() {
   # -s 진행내역, 메시지 출력x, -o 원격데이터를 /dev/null로보내 아무런 출력 x, http_code- response code(200인가 아닌가)
   if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 에러로 봄 - 정상코드 200
   then
-    CURRENT_PROFILE='real2'
+    CURRENT_PROFILE="real2"
   else
     CURRENT_PROFILE=$(curl -s http://localhost/profile)
   fi
 
-  if [${CURRENT_PROFILE} -eq 'real1' ]
+  if [${CURRENT_PROFILE} -eq "real1" ]
   then
-    IDLE_PROFILE='real2'
+    IDLE_PROFILE="real2"
   else
-    IDLE_PROFILE='real1'
+    IDLE_PROFILE="real1"
   fi
 
   echo "${IDLE_PROFILE}"
@@ -25,7 +25,7 @@ function find_idle_port()
 {
   IDLE_PROFILE=$(find_idle_profile)
 
-  if [ ${IDLE_PROFILE} == 'real1' ]
+  if [ ${IDLE_PROFILE} == "real1" ]
   then
     echo "8081"
   else
