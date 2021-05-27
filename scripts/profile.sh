e69768fe5645d25bba1989ec8a8296538bb3e5d7
5STARJeongHee <qhxmaoflr@gmail.com>
2021-05-27 17:23:23 +0900

    무중단 배포를 위한 script, properties 추가, 버전 명 날짜를 추가해 자동으로 버전이 바뀌고 겹치지 않도록함,

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 # 쉬고 있는 profile 찾기: real1 사용중일 경우 real2가 쉬고, 반대일 경우 real1이 쉬고, real1이 살아 있음
-
-fucntion find_idle_profile()
-{
-  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
-
+function find_idle_profile() {
+  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}\n" http://localhost/profile)
+  # -s 진행내역, 메시지 출력x, -o 원격데이터를 /dev/null로보내 아무런 출력 x, http_code- response code(200인가 아닌가)
   if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 에러로 봄 - 정상코드 200
   then
     CURRENT_PROFILE=real2
